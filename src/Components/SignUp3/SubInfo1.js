9565f79bf6b1491c78dc19533b3d96dfeed27a8f
Sonia <soniaotiono@gmail.com>
2021-03-12 15:37:43 +0100

    Dashboard modifications

--- src/Components/SignUp3/SubInfo1.js
+++ src/Components/SignUp3/SubInfo1.js
@@ -3,10 +3,12 @@ import React from 'react'
 const SubInfo1 = () => {
     return(
         <div className="click2">
-            <span className="cac2"> Everything you need to start your business</span>
-            <p><i className="fa fa-check fa-md"></i > Registered business with the CAC</p>
-            <p><i className="fa fa-check fa-md"></i > Tax identification number</p>
-            <p><i className="fa fa-check fa-md"></i > Your account will be automatically opened on completion</p>
+            <p className="mb-4 mt-0">
+                <span className="cac2 mt-0"> Everything you need to start your business</span>
+            </p>
+            <p className="mb-0"><i className="fa fa-check fa-md"></i > Registered business with the CAC</p>
+            <p className="mb-0"><i className="fa fa-check fa-md"></i > Tax identification number</p>
+            <p className="mb-"><i className="fa fa-check fa-md"></i > Your account will be automatically opened on completion</p>
         </div>
     )
 }
